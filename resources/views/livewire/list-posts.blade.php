770d827c98b2979c7231166fe534958916bc83c3
Miles Wu <slion0725@gmail.com>
2023-11-07 16:46:35 +0800

    upd

--- resources/views/livewire/list-posts.blade.php
+++ resources/views/livewire/list-posts.blade.php
@@ -1,6 +1,8 @@
 <div>
     <span>{{ session('status') }}</span>
 
+    <a href="{{ route('posts.create') }}">Create</a>
+
     <ul>
         @foreach ($posts as $post)
             <li><a href="{{ url('/posts/' . $post->id) }}">{{ $post->title }}</a></li>
